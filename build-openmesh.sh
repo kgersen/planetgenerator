de17951918ffb4b2f6f288418b277ba3fd98333f
kgersen <kgersen@hotmail.com>
2023-06-21 23:14:54 +0200

    - cmake for Windows

--- build-openmesh.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-prefix_dir="$(pwd)/OpenMesh"
-mkdir build-OpenMesh
-cd build-OpenMesh
-cmake ../../OpenMesh-9.0.0 -DBUILD_APPS=OFF -DCMAKE_INSTALL_PREFIX="$prefix_dir"
-make && make install
